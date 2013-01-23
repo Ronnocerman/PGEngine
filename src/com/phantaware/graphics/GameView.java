1a5cbe525896cef7121b7765726e3ea2944fbeb0
Connor Nicholson <Ronnocerman@yahoo.com>
2013-01-23 13:52:21 -0800

    Added addScreen and removeScreen

--- src/com/phantaware/graphics/GameView.java
+++ src/com/phantaware/graphics/GameView.java
@@ -1,5 +1,7 @@
 package com.phantaware.graphics;
 
+import java.util.ArrayList;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -9,9 +11,25 @@ import android.opengl.GLSurfaceView.Renderer;
 
 public class GameView extends GLSurfaceView implements Renderer
 {
-
+	private ArrayList<GameRenderer> screens = new ArrayList<GameRenderer>();
 	public GameView(Context context) {
 		super(context);
+	}
+	public void addScreen(GameRenderer r)
+	{
+		if(!screens.contains(r))
+			screens.add(r);
+	}
+	
+	public void removeScreen(GameRenderer r)
+	{
+		if(screens.contains(r))
+			screens.remove(r);
+	}
+	
+	public ArrayList<GameRenderer> getScreens()
+	{
+		return new ArrayList<GameRenderer>(screens);
 	}
 
 	public void onDrawFrame(GL10 gl) {
